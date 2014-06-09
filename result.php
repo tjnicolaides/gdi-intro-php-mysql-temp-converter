1b52338a3840d39de02d95a8d1437fb7af097d54
TJ Nicolaides <tjnicolaides@gmail.com>
2014-06-09 00:31:27 -0400

    starter files for temperature converter drill

---
+++ result.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <link href="assets/style.css" type="text/css" rel="stylesheet"/>
+        <title>PHP Class 1 Results</title>
+    </head>
+
+    <body>
+    
+        <div class="wrapper">
+            <div class="header">
+                <h1>Results Page</h1>
+            </div>
+            <div class="content">
+            
+            
+             <!--Input data from slides 22-25-->
+             
+    
+            </div>
+            <div class="footer">
+                <img src="assets/cc_by.png" alt="Creative Commons Licensed By" title="Creative Commons Licensed By"  height="45"/> Izzy Johnston, in conjunction with Girl Develop It! </div>
+        </div>
+    </body>
+</html>
